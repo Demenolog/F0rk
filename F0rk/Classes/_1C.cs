21b316f4e7455ca85a296783dbaddaea67d540a2
Denis Nikolenko <Demenolog0@yandex.ru>
2022-06-05 17:52:12 +0500

    Change 1C class

--- F0rk/Classes/_1C.cs
+++ F0rk/Classes/_1C.cs
@@ -5,31 +5,29 @@ namespace F0rk.Classes
 {
     public class _1C
     {
-        public static string[] GetPathsToClear()
+        private static readonly string[] PathsToClean =
         {
-            var paths = new string[]
-            {
-                Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Roaming\1C\1Cv8\"),
-                Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Local\1C\1Cv8\"),
-                Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Local\temp"),
-                @"C:\Contract\DATA\DB_CACHE\WORK\",
-                @"D:\Programs\Contract\DATA\DB_CACHE\WORK\",
-                @"D:\Profile\cms\AppData\Local\1C\1cv8"
-            };
-            return paths;
-        }
+            Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Roaming\1C\1Cv8\"),
+            Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Local\1C\1Cv8\"),
+            Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Local\temp"),
+            @"C:\Contract\DATA\DB_CACHE\WORK\",
+            @"D:\Programs\Contract\DATA\DB_CACHE\WORK\",
+            @"D:\Profile\cms\AppData\Local\1C\1cv8"
+        };
+
+        private static readonly string[] ApacheService =
+        {
+            "apache2.4",
+            "apache2.2"
+        };
+
+        public static string[] GetPathsToClear() => PathsToClean;
 
         public static void ApacheOff()
         {
-            var services = new string[]
-            {
-                "apache2.4",
-                "apache2.2"
-            };
-
             try
             {
-                foreach (string service in services)
+                foreach (string service in ApacheService)
                 {
                     var sv = new ServiceController(service);
                     sv.Stop();
@@ -43,15 +41,9 @@ namespace F0rk.Classes
 
         public static void ApacheOn()
         {
-            var services = new string[]
-            {
-                "apache2.4",
-                "apache2.2"
-            };
-
             try
             {
-                foreach (string service in services)
+                foreach (string service in ApacheService)
                 {
                     var sv = new ServiceController(service);
                     sv.Start();
