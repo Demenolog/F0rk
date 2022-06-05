cd141b12052678bd5a6becd4ef051c84b6420845
Denis Nikolenko <Demenolog0@yandex.ru>
2022-06-05 15:48:35 +0500

    Addeting some paths to 1C

--- F0rk/Classes/_1C.cs
+++ F0rk/Classes/_1C.cs
@@ -10,6 +10,7 @@ namespace F0rk.Classes
             {
                 Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Roaming\1C\1Cv8\"),
                 Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Local\1C\1Cv8\"),
+                Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Local\temp"),
                 @"C:\Contract\DATA\DB_CACHE\WORK\",
                 @"D:\Programs\Contract\DATA\DB_CACHE\WORK\",
                 @"D:\Profile\cms\AppData\Local\1C\1cv8"
