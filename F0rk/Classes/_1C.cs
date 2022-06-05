86ea3ffcb984dd1560462b4cf303d8022b687c07
Denis Nikolenko <Demenolog0@yandex.ru>
2022-06-05 15:36:42 +0500

    Addeting 1C class

---
+++ F0rk/Classes/_1C.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace F0rk.Classes
+{
+    public class _1C
+    {
+        public static string[] GetPathsToClear()
+        {
+            var paths = new string[]
+            {
+                Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Roaming\1C\1Cv8\"),
+                Environment.ExpandEnvironmentVariables(@"%USERPROFILE%\AppData\Local\1C\1Cv8\"),
+                @"C:\Contract\DATA\DB_CACHE\WORK\",
+                @"D:\Programs\Contract\DATA\DB_CACHE\WORK\",
+                @"D:\Profile\cms\AppData\Local\1C\1cv8"
+            }
+            return paths;
+        }
+    }
+}
