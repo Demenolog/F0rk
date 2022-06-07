083349967bbf0151f941ea7665e684375b6ff1df
Denis Nikolenko <Demenolog0@yandex.ru>
2022-06-07 22:10:13 +0500

    Add clean up method

---
+++ F0rk/Methods/DirectoryCleaner/DirectoryCleaner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F0rk.Methods.DirectoryCleaner
+{
+    public static class DirectoryCleaner
+    {
+        public static void CompleteCleanup(string[] paths)
+        {
+
+        }
+    }
+}
