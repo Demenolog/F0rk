4aeab80eaa4d19c3446082310ffb87ac1fd1e610
Denis Nikolenko <Demenolog0@yandex.ru>
2022-06-09 21:31:36 +0500

    add paths to clean to DisdD class

--- F0rk/Classes/DiskD.cs
+++ F0rk/Classes/DiskD.cs
@@ -2,6 +2,30 @@
 {
     internal static class DiskD
     {
+        private static readonly string[] PathsToCompleteClean =
+        {
+            @"C:\Program Files (x86)\InstallShield Installation Information",
+            @"C:\Windows\ccmsetup",
+            @"C:\Windows\Temp",
+            @"C:\Windows\ccmcache",
+            @"D:\Profile\cms\AppData\Local\Temp"
+        };
+
+        private static readonly string[] PathsToNotCompleteClean =
+        {
+            @"C:\Windows\SoftwareDistribution",
+            @"C:\\Windows\PCHEALTH",
+            @"C:\Windows\ServiceProfiles\LocalService\AppData\LocalLow\Microsoft\CryptnetUrlCache",
+            @"C:\Windows\System32\config\systemprofile\AppData\LocalLow\Microsoft\CryptnetUrlCache",
+            @"D:\Profile\cms\AppData\Local\Microsoft\Windows\WER"
+        };
+
+        private static readonly string[] Tasks =
+        {
+            "1cv8c.exe",
+            "httpd.exe"
+        };
+
         private static readonly string[] Services =
         {
             "BITS",
