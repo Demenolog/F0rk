cab0203f2d9ddd6908bacab1354e1d4748ee469e
Denis Nikolenko <Demenolog0@yandex.ru>
2022-06-22 21:43:17 +0500

    Create static class ctor for Disk.cs and 1C.cs

--- F0rk/Models/Classes/DiskD.cs
+++ F0rk/Models/Classes/DiskD.cs
@@ -4,52 +4,78 @@ namespace F0rk.Classes
 {
     internal static class DiskD
     {
-        private static readonly string PathToEmails = @"D:\Profile\cms\AppData\Local\Microsoft\Windows Live Mail\";
+        private static readonly string[] EmailTasksNames;
 
-        private static readonly string[] PathsesToCompleteClean =
-        {
-            @"C:\Program Files (x86)\InstallShield Installation Information",
-            @"C:\Windows\ccmsetup",
-            @"C:\Windows\Temp",
-            @"C:\Windows\ccmcache",
-            @"D:\Profile\cms\AppData\Local\Temp"
-        };
+        private static readonly string[] PathsesToCompleteClean;
 
-        private static readonly string[] PathsToSubfoldersClean =
-        {
-            @"C:\Windows\SoftwareDistribution",
-            @"C:\\Windows\PCHEALTH",
-            @"C:\Windows\ServiceProfiles\LocalService\AppData\LocalLow\Microsoft\CryptnetUrlCache",
-            @"C:\Windows\System32\config\systemprofile\AppData\LocalLow\Microsoft\CryptnetUrlCache",
-            @"D:\Profile\cms\AppData\Local\Microsoft\Windows\WER"
-        };
+        private static readonly string[] PathsToSubfoldersClean;
 
-        private static readonly string[] Tasks =
-        {
-            "1cv8c.exe",
-            "httpd.exe",
-            "iexplore.exe",
-            "chrome.exe",
-            "wmplayer.exe",
-            "soffice.exe",
-            "soffice.bin",
-            "scalc.exe",
-            "winmail.exe",
-            "swriter.exe"
-        };
+        private static readonly string PathToEmails;
 
-        private static readonly string[] EmailTasksNames =
-        {
-            "iexplore.exe",
-            "winmail.exe"
-        };
+        private static readonly string[] Services;
 
-        private static readonly string[] Services =
+        private static readonly string[] Tasks;
+
+        static DiskD()
         {
-            "bits",
-            "wuauserv",
-            "CryptSvc"
-        };
+            EmailTasksNames = new[]
+            {
+                "iexplore.exe",
+                "wlmail.exe"
+            };
+
+            PathsesToCompleteClean = new[]
+            {
+                @"C:\Windows\ccmsetup",
+                @"C:\Windows\Temp",
+                @"C:\Windows\ccmcache",
+                @"D:\Profile\cms\AppData\Local\Temp"
+            };
+
+            PathsToSubfoldersClean = new[]
+            {
+                @"C:\Windows\SoftwareDistribution",
+                @"C:\Windows\PCHEALTH",
+                @"C:\Windows\ServiceProfiles\LocalService\AppData\LocalLow\Microsoft\CryptnetUrlCache",
+                @"C:\Windows\System32\config\systemprofile\AppData\LocalLow\Microsoft\CryptnetUrlCache",
+                @"D:\Profile\cms\AppData\Local\Microsoft\Windows\WER"
+            };
+
+            PathToEmails = @"D:\Profile\cms\AppData\Local\Microsoft\Windows Live Mail\";
+
+            Services = new[]
+            {
+                "bits",
+                "wuauserv",
+                "CryptSvc"
+            };
+
+            Tasks = new[]
+            {
+                "1cv8c.exe",
+                "httpd.exe",
+                "iexplore.exe",
+                "chrome.exe",
+                "wmplayer.exe",
+                "soffice.exe",
+                "soffice.bin",
+                "scalc.exe",
+                "wlmail.exe",
+                "swriter.exe"
+            };
+        }
+
+        public static string[] GetCommonTasksNames => Tasks;
+
+        public static string[] GetEmailTasksNames => EmailTasksNames;
+
+        public static string[] GetPathsToCompleteClean => PathsesToCompleteClean;
+
+        public static string[] GetPathsToSubfoldersClean => PathsToSubfoldersClean;
+
+        public static string GetPathToEmails => PathToEmails;
+
+        public static string[] GetServicesToStop => Services;
 
         public static Process[][] GetTasksToKill(string[] tasks)
         {
@@ -62,19 +88,5 @@ namespace F0rk.Classes
 
             return processes;
         }
-
-        public static string[] GetCommonTasksNames => EmailTasksNames;
-
-        public static string[] GetEmailTasksNames => EmailTasksNames;
-
-        public static string[] GetServicesToStop => Services;
-
-        public static string[] GetPathsToCompleteClean => PathsesToCompleteClean;
-
-        public static string[] GetPathsToSubfoldersClean => PathsToSubfoldersClean;
-
-        public static string GetPathToEmails => PathToEmails;
-
-
     }
 }
--- F0rk/Models/Classes/_1C.cs
+++ F0rk/Models/Classes/_1C.cs
@@ -1,35 +1,44 @@
-﻿using System.Diagnostics;
-using F0rk.Methods.ServiceHandler;
+﻿using F0rk.Methods.ServiceHandler;
+using System.Diagnostics;
 
 namespace F0rk.Classes
 {
     public static class _1C
     {
-        private static readonly string[] PathsToClean =
-        {
-            @"D:\Profile\cms\AppData\Roaming\1C\1cv8\",
-            @"D:\Profile\cms\AppData\Local\Temp\",
-            @"D:\Profile\cms\AppData\Local\1C\1cv8\",
-            @"D:\Programs\Contract\DATA\DB_CACHE\WORK\"
-        };
+        private static readonly string[] ApacheService;
 
-        private static readonly string[] Tasks =
-        {
-            "1cv8c.exe",
-            "httpd.exe"
-        };
+        private static readonly string[] PathsToClean;
 
-        private static readonly string[] ApacheService =
-        {
-            "apache2.4",
-            "apache2.2"
-        };
+        private static readonly string[] Tasks;
 
-        public static string[] GetPathsToClear() => PathsToClean;
+        static _1C()
+        {
+            PathsToClean = new[]
+            {
+                @"D:\Profile\cms\AppData\Roaming\1C\1cv8",
+                @"D:\Profile\cms\AppData\Local\Temp",
+                @"D:\Profile\cms\AppData\Local\1C\1cv8",
+                @"D:\Programs\Contract\Data\DB_CACHE\WORK"
+            };
+
+            Tasks = new[]
+            {
+                "1cv8c.exe",
+                "httpd.exe"
+            };
+
+            ApacheService = new[]
+            {
+                "apache2.4",
+                "apache2.2"
+            };
+        }
+
+        public static void ApacheStart() => ServiceHandler.ServicesStart(ApacheService);
 
         public static void ApacheStop() => ServiceHandler.ServicesStop(ApacheService);
 
-        public static void ApacheStart() => ServiceHandler.ServicesStart(ApacheService);
+        public static string[] GetPathsToClear() => PathsToClean;
 
         public static Process[][] GetTasksToKill()
         {
