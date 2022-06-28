ae6a619cb849e315517c1ac31807f99e3404ddcc
Denis Nikolenko <Demenolog0@yandex.ru>
2022-06-28 22:19:31 +0500

    addeting StartTask method to class Task Handler.cs

--- F0rk/Models/Methods/TasksHandler/TasksHandler.cs
+++ F0rk/Models/Methods/TasksHandler/TasksHandler.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Diagnostics;
-using System.Windows;
+using System.IO;
 
 namespace F0rk.Models.Methods.TasksHandler
 {
@@ -38,6 +38,31 @@ namespace F0rk.Models.Methods.TasksHandler
                 // ignore
             }
         }
-    }
 
+        public static void StartTask(string filename, string[] commands)
+        {
+            using (var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = filename + ".exe",
+                    RedirectStandardInput = true,
+                    UseShellExecute = false
+                }
+            })
+            {
+                process.Start();
+                using (StreamWriter pWriter = process.StandardInput)
+                {
+                    if (pWriter.BaseStream.CanWrite)
+                    {
+                        foreach (string command in commands)
+                        {
+                            pWriter.WriteLine(command);
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
