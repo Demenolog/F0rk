65ef171f35648bf53607e8676e6d21a7340a9958
Denis Nikolenko <Demenolog0@yandex.ru>
2022-06-29 22:27:45 +0500

    Making some tests

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -12,9 +12,41 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            string sapUrl = @"http://127.0.0.1:8200/";
+            var PinpadServiceName = "Upos2Agent";
 
-            Process.Start("chrome.exe", sapUrl + " -incognito");
+            var UnregistrationCommands = new[]
+            {
+                @"sc delete Upos2Agent",
+                @"regsvr32 /u D:\Programs\UPOS\sbrf.dll",
+                @"regsvr32 /u D:\Programs\UPOS\SBRFCOM.dll"
+            };
+
+            var RegistrationCommands = new[]
+            {
+                @"D:\Programs\UPOS\Agent.exe /reg",
+                @"regsvr32 D:\Programs\UPOS\sbrf.dll",
+                @"regsvr32 D:\Programs\UPOS\SBRFCOM.dll"
+            };
+
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = PinpadServiceName,
+                    RedirectStandardInput = true,
+                    UseShellExecute = false
+                }
+            };
+            process.Start();
+
+            StreamWriter pWriter = process.StandardInput;
+            if (pWriter.BaseStream.CanWrite)
+            {
+                foreach (string command in UnregistrationCommands)
+                {
+                    pWriter.WriteLine(command);
+                }
+            }
         }
 
     }
