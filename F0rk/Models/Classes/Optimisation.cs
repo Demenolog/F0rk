072a2eb351fba5833dc151aaaf1f1ebbfaa2ce99
Denis Nikolenko <Demenolog0@yandex.ru>
2022-07-05 22:30:23 +0500

    Change increasepagefile method

--- F0rk/Models/Classes/Optimisation.cs
+++ F0rk/Models/Classes/Optimisation.cs
@@ -12,15 +12,15 @@ namespace F0rk.Models.Classes
         {
             WmicPagefileIncreaseCommands = new[]
             {
-                @"computersystem set AutomaticManagedPagefile=False /NOINTERACTIVE",
-                @"pagefileset delete /NOINTERACTIVE",
-                @"pagefileset create name=""C:\\pagefile.sys"" /NOINTERACTIVE",
-                @"pagefileset where name=""C:\\\\pagefile.sys"" set InitialSize=3546,MaximumSize=3546 /NOINTERACTIVE",
-                @"pagefileset create name=""D:\\pagefile.sys"" /NOINTERACTIVE",
-                @"pagefileset where name=""D:\\\\pagefile.sys"" set InitialSize=3546,MaximumSize=3546 /NOINTERACTIVE"
+                @"wmic computersystem set AutomaticManagedPagefile=False /NOINTERACTIVE",
+                @"wmic pagefileset delete /NOINTERACTIVE",
+                @"wmic pagefileset create name=""C:\\pagefile.sys"" /NOINTERACTIVE",
+                @"wmic pagefileset where name=""C:\\\\pagefile.sys"" set InitialSize=3546,MaximumSize=3546 /NOINTERACTIVE",
+                @"wmic pagefileset create name=""D:\\pagefile.sys"" /NOINTERACTIVE",
+                @"wmic pagefileset where name=""D:\\\\pagefile.sys"" set InitialSize=3546,MaximumSize=3546 /NOINTERACTIVE"
             };
         }
 
-        public static void IncreasePagefile() => TasksHandler.StartTaskWithCommands("wmic.exe", WmicPagefileIncreaseCommands);
+        public static void IncreasePagefile() => TasksHandler.StartTaskWithCommands("cmd.exe", WmicPagefileIncreaseCommands);
     }
 }
