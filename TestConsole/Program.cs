c4d715df8954ff4e1c87369e6924f15d238db0f7
Denis Nikolenko <Demenolog0@yandex.ru>
2022-08-08 21:15:29 +0500

    [TEST CONSOLE]

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System.IO;
 using System.Text;
 using F0rk.Models.Methods.TasksHandler;
+using F0rk.Models.Methods.ServiceHandler;
 
 namespace TestConsole
 {
@@ -8,35 +9,14 @@ namespace TestConsole
     {
         private static void Main(string[] args)
         {
-            string path = @"C:\Windows\Svyaznoy\TimeSynchronization";
-
-            string[] TimeSynchronization = new[]
+            var Services = new[]
             {
-                @"sc start W32Time",
-                @"sc config W32Time start=auto",
-                @"W32tm.exe /resync"
+                "bits",
+                "wuauserv",
+                "CryptSvc"
             };
 
-            string[] TimeSynchronizationCommandForCmd = new[]
-            {
-                    @"SCHTASKS /Create /SC ONSTART /TN TimeSynchronization /TR C:\Windows\Svyaznoy\TimeSynchronization\TimeSynchronization.bat"
-            };
-
-            Directory.CreateDirectory(path);
-
-            FileInfo fi = new FileInfo(path + "\\TimeSynchronization.bat");
-
-            using (var fs = fi.CreateText())
-            {
-                foreach (string command in TimeSynchronization)
-                {
-                    fs.WriteLine(command);
-                }
-            }
-
-            TasksHandler.StartTaskWithCommands("cmd", TimeSynchronization);
-
-            TasksHandler.StartTaskWithCommands("cmd", TimeSynchronizationCommandForCmd);
+            ServiceHandler.ServiceStop(Services);
         }
     }
 }
