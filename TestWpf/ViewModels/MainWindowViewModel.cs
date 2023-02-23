35c94106ff80cddff8cdd3c304f20f191a54ab62
Denis Nikolenko <Demenolog0@yandex.ru>
2023-02-23 19:53:44 +0500

    [TestWpf]

---
+++ TestWpf/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using TestWpf.ViewModels.Base;
+
+namespace TestWpf.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region MainWindow Title
+
+        private string _title = "Test wpf window";
+
+        public string Title
+        {
+            get => _title;
+            set => SetField(ref _title, value);
+        }
+
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            
+        }
+    }
+}
