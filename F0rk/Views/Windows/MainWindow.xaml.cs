3d52d75bd115f30836b75f2bce138cd282458af7
Denis Nikolenko <Demenolog0@yandex.ru>
2023-02-26 15:10:54 +0500

    Some changes

--- F0rk/Views/Windows/MainWindow.xaml.cs
+++ F0rk/Views/Windows/MainWindow.xaml.cs
@@ -3,15 +3,11 @@ using System.Windows;
 
 namespace F0rk.Views.Windows
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-
     }
 }
